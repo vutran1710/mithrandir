1c2a2dea8dd96a1c6db59465a42da043ec7024bb
vutr <me@vutr.io>
2020-10-19 22:22:07 +0700

    add files

---
+++ scripts.py
@@ -0,0 +1,23 @@
+from subprocess import check_call
+
+
+def lint() -> None:
+    check_call(["flake8", "pyrate_limiter/"])
+    check_call(["pylint", "pyrate_limiter/", "--rcfile=setup.cfg"])
+    check_call(["autopep8", "pyrate_limiter/*.py"])
+
+
+def test() -> None:
+    check_call(
+        [
+            "pytest",
+            "tests",
+            "--maxfail=1",
+            "--verbose",
+            "-s",
+            "--fulltrace",
+            "--cov-report",
+            "html",
+            "--cov=pyrate_limiter",
+        ]
+    )
